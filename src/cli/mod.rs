344a7284c725082439356d9b8c0dcda8aab3826d
Mathis <eon@abes.fr>
2021-01-31 19:38:17 +0100

    Add cli

---
+++ src/cli/mod.rs
@@ -0,0 +1,72 @@
+use clap::{
+    crate_authors, crate_description, crate_name, crate_version, App, Arg, ArgMatches, SubCommand,
+};
+
+pub fn cli() -> ArgMatches<'static> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .subcommand(
+            SubCommand::with_name("check").about("Checks IDs").arg(
+                Arg::with_name("input")
+                    .short("i")
+                    .long("input")
+                    .multiple(true)
+                    .min_values(1)
+                    .value_name("FILE")
+                    .help("Checks a list of IDs stored in one or more files. One ID per line"),
+            )
+            .arg(
+                Arg::with_name("output")
+                    .short("o")
+                    .long("output")
+                    .value_name("FILE")
+                    .help("Output file")
+                )
+            .arg(
+                Arg::with_name("workers")
+                    .short("w")
+                    .long("workers")
+                    .value_name("NUMBER OF WORKERS")
+                    .default_value("4")
+                    .help("Sets the number of workers")
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("checksum").about("Computes the checksum char of IDs").arg(
+                Arg::with_name("input")
+                    .short("i")
+                    .long("input")
+                    .multiple(true)
+                    .min_values(1)
+                    .value_name("FILE")
+                    .help("Computes the checksum char of a list of IDs stored in one or more file. One ID per line")
+                )
+                .arg(
+                    Arg::with_name("output")
+                        .short("o")
+                        .long("output")
+                        .value_name("FILE")
+                        .help("Output file")
+                )
+                .arg(
+                    Arg::with_name("workers")
+                        .short("w")
+                        .long("workers")
+                        .value_name("NUMBER OF WORKERS")
+                        .default_value("4")
+                        .help("Sets the number of workers")
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("ws").about("Starts the NCDA Web Service").arg(
+                Arg::with_name("port")
+                    .short("p")
+                    .long("port")
+                    .value_name("PORT")
+                    .help("Sets the port used by the Web Service"),
+            ),
+        )
+        .get_matches()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,164 @@
+#![feature(test)]
 #![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
 
-mod ncda;
+use std::error::Error;
+use std::path::Path;
+use std::path::PathBuf;
+
+#[macro_use]
+extern crate rocket;
+
+use futures::StreamExt;
+use glob;
+use tokio::fs::File;
+use tokio::io::{self, AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufWriter};
+
+mod cli;
+mod consts;
 mod errors;
+mod ncda;
 mod ws;
 
-fn main() {
-    // for _i in 0..40_000_000 {
-    //     ncda::check("cb32752361d");
-    // }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let matches = cli::cli();
 
-    ws::launch_ws(None);
+    if let Some(ref matches) = matches.subcommand_matches("check") {
+        let workers = set_workers(matches.value_of("workers"));
+
+        if let Some(files) = matches.values_of("input") {
+            let files = file_list(files)?;
+
+            let cb = move |input: &str, file_path: &PathBuf, line_number: usize| -> String {
+                match ncda::check(&input) {
+                    Ok(()) => format!("{}:{}  {:<31} ok", file_path.display(), line_number, &input),
+                    Err(e) => format!(
+                        "{}:{}  {:<31} {}",
+                        file_path.display(),
+                        line_number,
+                        &input,
+                        e.to_string()
+                    ),
+                }
+            };
+
+            async_file_processing(files, workers, cb, matches.value_of("output")).await?;
+        }
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("checksum") {
+        let workers = set_workers(matches.value_of("workers"));
+
+        if let Some(files) = matches.values_of("input") {
+            let files = file_list(files)?;
+
+            let cb = move |input: &str, file_path: &PathBuf, line_number: usize| -> String {
+                match ncda::checksum(&input) {
+                    Ok(check_char) => format!(
+                        "{}:{}  {}{}",
+                        file_path.display(),
+                        line_number,
+                        &input,
+                        check_char
+                    ),
+                    Err(e) => format!(
+                        "{}:{}  {:<31} {}",
+                        file_path.display(),
+                        line_number,
+                        &input,
+                        e.to_string()
+                    ),
+                }
+            };
+
+            async_file_processing(files, workers, cb, matches.value_of("output")).await?;
+        }
+    }
+
+    if let Some(ref matches) = matches.subcommand_matches("ws") {
+        ws::launch_ws(matches.value_of("port").map(|v| {
+            v.parse::<u16>()
+                .expect(&format!("{} is not a valid port number", v))
+        }))?;
+    }
+
+    Ok(())
+}
+
+fn file_list<'a, S: Iterator<Item = &'a str>>(input: S) -> Result<Vec<PathBuf>, glob::GlobError> {
+    let patterns = input
+        .into_iter()
+        .map(|file_path| {
+            glob::glob(&file_path)
+                .into_iter()
+                .flat_map(|path| path)
+                .collect::<Result<Vec<PathBuf>, glob::GlobError>>()
+        })
+        .collect::<Result<Vec<Vec<PathBuf>>, glob::GlobError>>()?;
+
+    Ok(patterns
+        .into_iter()
+        .flat_map(|file_path| file_path)
+        .filter(|file_path| file_path.is_file())
+        .collect::<Vec<PathBuf>>())
+}
+
+async fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename.as_ref()).await?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+async fn async_file_processing(
+    input: Vec<PathBuf>,
+    workers: usize,
+    cb: impl FnOnce(&str, &PathBuf, usize) -> String + Copy,
+    output: Option<&str>,
+) -> Result<(), Box<dyn Error>> {
+    futures::stream::iter({
+        input.into_iter().map(|entry| async move {
+            let mut writer: BufWriter<Box<dyn AsyncWrite + Unpin>> = match output {
+                Some(output_file) => BufWriter::new(Box::new(
+                    File::create(output_file)
+                        .await
+                        .expect("failed to create output file"),
+                )),
+                None => BufWriter::new(Box::new(io::stdout())),
+            };
+
+            let mut lines = read_lines(&entry).await.unwrap();
+
+            let mut line_number: usize = 0;
+
+            while let Some(line_content) = lines.next_line().await.expect("failed to get next line")
+            {
+                let mut r = cb(&line_content, &entry, line_number.clone());
+                line_number += 1;
+                r.push_str("\n");
+
+                writer
+                    .write_all(&mut r.as_bytes())
+                    .await
+                    .expect("failed to write data")
+            }
+
+            writer.shutdown().await.expect("failed to write data");
+        })
+    })
+    .buffer_unordered(workers)
+    .collect::<Vec<()>>()
+    .await;
+
+    Ok(())
+}
+
+fn set_workers(input: Option<&str>) -> usize {
+    match input {
+        Some(workers) => workers
+            .parse::<usize>()
+            .expect(&format!("{} is not a valid number of workers", workers)),
+        None => consts::WORKERS,
+    }
 }
