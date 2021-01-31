bb66f709a3d03dc692e4ff2c4296d9e2eb6a17eb
Mathis <eon@abes.fr>
2021-01-31 19:41:06 +0100

    cargo fmt

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,10 +2,18 @@ use ncda;
 
 fn main() -> Result<(), ncda::NcdaError> {
     let unqualified_id = "cb32752361";
-    println!("The checksum char of {} is : {:?}", unqualified_id, ncda::checksum(unqualified_id)?);
-    
+    println!(
+        "The checksum char of {} is : {:?}",
+        unqualified_id,
+        ncda::checksum(unqualified_id)?
+    );
+
     let qualified_id = "cb32752361d";
-    println!("{} is valid : {:?}", qualified_id, ncda::check(qualified_id).is_ok());
+    println!(
+        "{} is valid : {:?}",
+        qualified_id,
+        ncda::check(qualified_id).is_ok()
+    );
 
     Ok(())
 }
