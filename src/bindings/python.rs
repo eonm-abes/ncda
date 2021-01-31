bc21f0bd8e81d4b2de750b33366888d779e74410
Mathis <eon@abes.fr>
2021-01-31 19:41:19 +0100

    cargo fmt

--- src/bindings/python.rs
+++ src/bindings/python.rs
@@ -1,14 +1,16 @@
-use crate::ncda;
 use crate::errors::NcdaError;
+use crate::ncda;
 
 use pyo3::prelude::*;
 
 impl std::convert::From<NcdaError> for pyo3::PyErr {
     fn from(err: NcdaError) -> Self {
         match err {
-            NcdaError::InvalidChecksum(_) => pyo3::exceptions::PyAssertionError::new_err(err.to_string()),
-            _ => pyo3::exceptions::PyValueError::new_err(err.to_string())
-        }   
+            NcdaError::InvalidChecksum(_) => {
+                pyo3::exceptions::PyAssertionError::new_err(err.to_string())
+            }
+            _ => pyo3::exceptions::PyValueError::new_err(err.to_string()),
+        }
     }
 }
 
@@ -21,7 +23,7 @@ pub fn checksum(input: &str) -> Result<char, NcdaError> {
 pub fn check(input: &str) -> PyResult<bool> {
     let x = match ncda::check(input) {
         Ok(_) => Ok(true),
-        Err(e) => Err(e)
+        Err(e) => Err(e),
     };
 
     Ok(x?)
