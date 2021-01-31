d72e6634fc0ccc50c6797e6ab74b9e452a104a71
Mathis <eon@abes.fr>
2021-01-31 19:40:29 +0100

    Sized Hashmaps

--- src/ws.rs
+++ src/ws.rs
@@ -1,78 +1,78 @@
 use crate::ncda;
+use std::error::Error;
 
 use rocket::config::{Config, Environment};
-use serde_json::{Value};
 use rocket_contrib::json::Json;
+use serde_json::Value;
 use std::collections::HashMap;
-
 
 // http://0.0.0.0:8080/v1/check?ids=cb32752361d,cb32752361d,cb32752361d
 #[get("/v1/check?<ids>")]
 fn check(ids: String) -> Json<Vec<Value>> {
-    let ids = ids.split(",").into_iter().map(|id| {
-        match ncda::check(id) {
+    let ids = ids
+        .split(",")
+        .into_iter()
+        .map(|id| match ncda::check(id) {
             Ok(()) => {
-                let mut map = HashMap::new();
+                let mut map = HashMap::with_capacity(1);
 
                 map.insert(id, true);
 
                 serde_json::to_value(&map).expect("Failed to deserialize data")
-            },
+            }
             Err(e) => {
-                let mut map = HashMap::new();
+                let mut map = HashMap::with_capacity(1);
 
                 map.insert(id, e);
 
                 serde_json::to_value(&map).expect("Failed to deserialize error")
             }
-        }
-    }).collect::<Vec<Value>>();
+        })
+        .collect::<Vec<Value>>();
 
     Json(ids)
 }
 
-// http://0.0.0.0:8080/v1/checksum?ids=cb32752361d,cb32752361d,cb32752361d
+// http://0.0.0.0:8080/v1/checksum?ids=cb32752361,cb32752361,cb32752361
 #[get("/v1/checksum?<ids>")]
 fn checksum(ids: String) -> Json<Vec<Value>> {
-    let result = ids.split(",").into_iter().map(|id| {
-        
-        match ncda::checksum(id) {
+    let result = ids
+        .split(",")
+        .into_iter()
+        .map(|id| match ncda::checksum(id) {
             Ok(checksum_char) => {
-                let mut map = HashMap::new();
+                let mut map = HashMap::with_capacity(1);
 
                 map.insert(id, checksum_char);
 
                 serde_json::to_value(&map).expect("Failed to deserialize data")
-            },
+            }
             Err(e) => {
-                let mut map = HashMap::new();
+                let mut map = HashMap::with_capacity(1);
 
                 map.insert(id, e);
 
                 serde_json::to_value(&map).expect("Failed to deserialize error")
             }
-        }
-    }).collect::<Vec<Value>>();
+        })
+        .collect::<Vec<Value>>();
 
     Json(result)
 }
 
-pub fn launch_ws(port: Option<u16>) {
+pub fn launch_ws(port: Option<u16>) -> Result<(), Box<dyn Error>> {
     let port = match port {
         Some(p) => p,
-        None => 8080
+        None => 8080,
     };
 
     let config = Config::build(Environment::Production)
         .port(port)
-        .finalize().unwrap();
+        .finalize()?;
 
     let rocket_ws = rocket::custom(config);
 
-    rocket_ws
-        .mount(
-            "/",
-            routes![check, checksum],
-        )
-        .launch();
+    rocket_ws.mount("/", routes![check, checksum]).launch();
+
+    Ok(())
 }
