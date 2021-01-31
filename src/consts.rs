e1448288409011b1061a2b326ca22b9fb0094d96
Mathis <eon@abes.fr>
2021-01-31 19:39:13 +0100

    Adding consts

---
+++ src/consts.rs
@@ -0,0 +1,6 @@
+pub const WORKERS: usize = 4;
+
+pub const CHARS: [char; 29] = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm',
+    'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z',
+];
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 #![feature(test)]
 //! # NCDA (Noid Check Digit Algorithm)
-//! 
-//! 
-//! 
+//!
+//!
+//!
 
 extern crate test;
 
-mod ncda;
 mod bindings;
 mod errors;
+mod ncda;
+mod consts;
 
 pub use bindings::*;
 pub use errors::*;
